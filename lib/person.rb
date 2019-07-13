10ab1df1b11bc504476a82df737ae9aa334e1322
Tiffany Tang <tifftang22@gmail.com>
2019-07-13 23:51:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
   def name=(person_name)
-    @new_person_name = person_name
+    @name = person_name
   end
 
   def name
-    @new_person_name
+    @name
   end
 
   def job=(person_job)
-    @new_person_job = person_job
+    @job = person_job
   end
 
   def job
-    @new_person_job
+    @job
   end
 end
